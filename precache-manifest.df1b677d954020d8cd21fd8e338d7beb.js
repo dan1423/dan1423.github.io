c841d0f1d1be49f44b7316a0372d4d4542039edb
dan1423 <oluwadare.daniel21@gmail.com>
2020-09-02 14:30:57 -0500

    Updates

---
+++ precache-manifest.df1b677d954020d8cd21fd8e338d7beb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3010667734bbe7f8d9e70cc25199b128",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "e41dc4d7f74fc193e6f9",
+    "url": "/expense-tracker-react/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "a3886eb48eeb822dea54",
+    "url": "/expense-tracker-react/static/js/2.abb637fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker-react/static/js/2.abb637fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e41dc4d7f74fc193e6f9",
+    "url": "/expense-tracker-react/static/js/main.beabe889.chunk.js"
+  },
+  {
+    "revision": "27d5a3916d5c0aa966b2",
+    "url": "/expense-tracker-react/static/js/runtime-main.69aaf8cd.js"
+  }
+]);
